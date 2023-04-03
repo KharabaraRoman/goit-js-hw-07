fd169c16b30245042473e302264d1bd608a18071
Роман Харабара <117296212+KharabaraRoman@users.noreply.github.com>
2023-04-03 15:16:14 +0200

    7 Ок

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,51 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+
+const galleryListEl = document.querySelector('.gallery');
+galleryListEl.insertAdjacentHTML('beforeend', createGalleryItem(galleryItems));
+
+function createGalleryItem(galleryItems) {
+  return galleryItems
+    .map(
+      ({ preview, original, description }) =>
+        `<li class="gallery__item">
+          <a class="gallery__link" href="${original}">
+            <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}">
+          </a>
+        </li>`,
+    )
+    .join('');
+}
+
+galleryListEl.addEventListener('click', (event) => {
+  event.preventDefault();
+  if (!event.target.classList.contains('gallery__image')) {
+    return;
+  }
+
+  const modalWindowImg = basicLightbox.create(`
+    <img src="${event.target.dataset.source}" width="800" height="600">
+  `);
+
+  modalWindowImg.show();
+
+  const closeOriginalImgByClick = () => {
+    modalWindowImg.close();
+    modalWindowImg.element().removeEventListener('click', closeOriginalImgByClick);
+    window.removeEventListener('keydown', closeOriginalImgByBtn);
+  };
+
+  const closeOriginalImgByBtn = (event) => {
+    if (event.code === 'Escape') {
+      modalWindowImg.close();
+      modalWindowImg.element().removeEventListener('keydown', closeOriginalImgByBtn);
+      window.removeEventListener('click', closeOriginalImgByClick);
+    }
+  };
+
+  modalWindowImg.element().addEventListener('click', closeOriginalImgByClick);
+  window.addEventListener('keydown', closeOriginalImgByBtn);
+});
+
+
+
